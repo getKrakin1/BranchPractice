cfc794890dfd33f809a10cca2b3bbb0377256afa
Jianing Wang <24wangj@gmail.com>
2023-01-13 22:25:05 -0500

    Improved motor method

--- src/main/java/frc/robot/subsystems/TestSubsystem.java
+++ src/main/java/frc/robot/subsystems/TestSubsystem.java
@@ -7,6 +7,7 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class TestSubsystem extends SubsystemBase {
@@ -17,11 +18,14 @@ public class TestSubsystem extends SubsystemBase {
   public TestSubsystem() {}
 
   public void runMotor() {
-    motor1.set(0.5);
+    if (motor1.isFollower()) {
+      motor1.set(0.5);
+    }
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Motor Output", motor1.getOutputCurrent());
   }
 }
