77cab934fef887e6ac8840783f7e47b3df858e81
Jianing Wang <24wangj@gmail.com>
2023-01-13 22:29:36 -0500

    Added best subsystem

---
+++ src/main/java/frc/robot/subsystems/BestSubsystem.java
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class BestSubsystem extends SubsystemBase {
+
+CANSparkMax motor2 = new CANSparkMax(1, MotorType.kBrushless);
+
+  /** Creates a new BestSubsystem. */
+  public BestSubsystem() {}
+
+  public void setCurrentLimit() {
+    motor2.setSmartCurrentLimit(100);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
